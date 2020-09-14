a5826cd6a914700b4c1f36b499020fc77c415667
sars33 <sergetester33@mail.com>
2020-09-14 21:24:41 +0300

    Java pre-project.Practical task 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,60 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    private static Connection connection;
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        UserService userService = new UserServiceImpl();
+
+      //  userService.createUsersTable();
+        addUserToDatabase();
+        showAllUsers();
+       // userService.cleanUsersTable();
+      //  userService.dropUsersTable();
+
     }
+
+    public static void addUserToDatabase() throws SQLException, ClassNotFoundException {
+        UserService userService = new UserServiceImpl();
+        User user = new User("John", "Black", (byte) 45);
+        userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+        System.out.println("User с именем " + user.getName() + " добавлен в базу данных");
+
+        User user2 = new User("Peter", "Krog", (byte) 28);
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        System.out.println("User с именем " + user2.getName() + " добавлен в базу данных");
+
+        User user3 = new User("Maria", "Garcia", (byte) 35);
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        System.out.println("User с именем " + user3.getName() + " добавлен в базу данных");
+
+        User user4 = new User("Andrei", "Matveev", (byte) 44);
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        System.out.println("User с именем " + user4.getName() + " добавлен в базу данных");
+    }
+
+    public static void showAllUsers() throws SQLException, ClassNotFoundException {
+        UserService userService = new UserServiceImpl();
+        userService.getAllUsers();
+        List<User> users = userService.getAllUsers();
+        System.out.println(users);
+    }
+
 }
+
+
+
+
+
+
